aecb5affd1844599101c0f5d9f6afb35faa29def
ChenNini0924 <162774572+ChenNini0924@users.noreply.github.com>
2024-03-10 22:10:22 +0800

    Update main.c

--- C/main.c
+++ C/main.c
@@ -8,255 +8,153 @@
 #include <Windows.h>
 
 #include "mione.h"
+#include "err.h"
 
-int getWordType(char** var, char word) {
-    *var = "\0";
-    if (word == '"' || word == '\'') {
-        *var = "'";
-        return 1;
-    }
-
-
-    if (word == ' ') {
-        *var = " ";
-        return 1;
-    }
-
-    if (word == '=') {
-        *var = "=";
-        return 1;
-    }
+int getWordType(int* var, char word) {
+	*var = "\0";
+	if (word == '"' || word == '\'') {
+		*var = 2;
+		return 1;
+	}
 
 
-    for (int i = 0; i < 26; i++) {
-        char a;
-        a = 64 + i;
-
-        char b;
-        b = 96 + i;
-
-      
-        if (word == a || word == b) {
-            *var = "abc";
-            return 1;
-        }
-        
-    }
-
-    for (int i = 0; i < 10; i++) {
-        char aw[5];
-        sprintf(aw, "%d", i);
-
-        if (aw) {
-            if (word == aw[0]) {
-                *var = "123";
-                return 1;
-            }
-        }
-    }
-
-    if (word == '{' || word == '}') {
-        *var = "table";
-        return 1;
-    }
+	if (word == ' ') {
+		*var = -1;
+		return 1;
+	}
 
 
-    if (word == '$' || word == '@' || word == '#') {
-        *var = "function";
-        return 1;
-    }
+
+	for (int i = 0; i < 26; i++) {
+		char a;
+		a = 65 + i;
+		char b;
+		b = 97 + i;
+
+	  
+		if (word == a || word == b) {
+			*var = 6;
+			return 1;
+		}
+		
+	}
+
+	for (int i = 0; i < 10; i++) {
+		char aw[5];
+		sprintf(aw, "%d", i);
+
+		if (aw) {
+			if (word == aw[0]) {
+				*var = 5;
+				return 1;
+			}
+		}
+	}
+
+	if (word == '{') {
+		*var = 3;
+		return 1;
+	}
+	if (word == '}') {
+		*var = 8;
+		return 1;
+	}
 
 
-    return 2;
+	if (word == '$') {
+		*var = 4;
+		return 1;
+	}
+	if (word == '@' || word == '#') {
+		*var = 9;
+		return 1;
+	}
+
+
+	return 2;
 };
 
 int OPEN(char* fileName) {
-    FILE* file = fopen(fileName, "r");
-    char line[2048];
+	FILE* file = fopen(fileName, "r");
+	char line[2048];
 
-    if (file == NULL) {
-        printf("cant open file\n");
-        return 1;
-    }
-
-    // "Hello World"
-    char inString = '\0';
-    char nextInString = 'n';
-
-    // $ ... @ | $ ... #
-    int inFunction = 0;
-    int nextInFunction = 2;
-
-    //{x,y,z}
-    int inTable = 0;
-    int nextInTable = 2;
-
-    // (x,y,z)
-    int inGather = 0;
-    int nextInGether = 2;
-
-    int inNumber = 0;
-    int nextImNumber = 2;
-
-    char* lastWordType = " ";
-
-    char* txt = malloc(2 * sizeof(char));
-    strcpy(txt, " ");
-
-    while (fgets(line, sizeof(line), file) != NULL) { //     C
-        //  printf("%s\n",line);
-
-        inString = '\0';
-        nextInString = 'n';
+	if (file == NULL) {
+		printf("cant open file\n");
+		return 1;
+	}
 
 
-        int lastOne = strlen(line) - 1;
+	while (fgets(line, sizeof(line), file) != NULL) { //     C
+		//  printf("%s\n",line);
 
-        for (int i = 0; strlen(line) > i; i++) { // C
-            if (i == lastOne) {
-                strcat(line, " ");
-            }
+		int lastOne = strlen(line) - 1;
 
-            char word = line[i];
-            char* wordType;
+		for (int i = 0; strlen(line) > i; i++) { // C
+			if (i == lastOne) {
+				strcat(line, " ");
+			}
 
-            getWordType(&wordType, word);
-            //特殊。偵測數字前面是否是字元
+			char word = line[i];
+			int wordType = 1;
+			
+			getWordType(&wordType, word);
 
-            if (wordType == "123") {
-                char* aw;
-                getWordType(&aw, line[i - 1]);
-                if (aw == "abc" || lastWordType == "abc") {
-                    wordType = "abc";
-                }
-            }
-            //
-           
+			mione(word, wordType);
 
+		}
 
-            if (nextInString == 'n') { //
-                if (wordType == "'") {
-                    if (inString != '\0') {
-                        nextInString = '\0';
-                    }
-                    else {
-                        inString = word;
-                    }
-                }
+		
+	}
 
 
-            }
-            else {
-                inString = nextInString;
-                nextInString = 'n';
-            }
+	
 
 
-
-
-
-            if (nextInTable == 2) {
-                if (wordType == "table") {
-                    if (inTable == 1) {
-                        nextInTable = 0;
-                    }
-                    else if (inTable == 0) {
-                        inTable = 1;
-                    }
-                }
-
-
-            }
-            else {
-                inTable = 0;
-                nextInTable = 2;
-            }
-
-
-
-
-
-
-            //=========================================================================
-            if (inString == '\0') {
-
-            }
-            else {
-                wordType = "'";
-            }
-            if (inTable == '\0') {
-
-            }
-            else {
-                wordType = "table";
-            }
-
-            char a[] = { word,'\0' };
-            txt = realloc(txt, sizeof(char) * (strlen(txt) + strlen(a)+1));
-
-            if (lastWordType == wordType ) {
-                
-                strcat(txt, a);
-            }
-            else {
-                
-                mione(txt);
-                strcpy(txt, a);
-
-            }
-
-           
-
-
-            lastWordType = wordType;
-        }
-    }
-
-    fclose(file);
-    return 0;
+	fclose(file);
+	return 0;
 }
 
 int main() {
-    LPWSTR* cmds;
-    int num;
-    cmds = CommandLineToArgvW(GetCommandLineW(), &num);
-    char m[256];
+	LPWSTR* cmds;
+	int num;
+	cmds = CommandLineToArgvW(GetCommandLineW(), &num);
+	char m[256];
 
-    if (num >= 2) {
-        if (wcscmp(cmds[1], L"o") == 0) {
-            WideCharToMultiByte(CP_ACP, 0, cmds[2], -1, m, sizeof(m), NULL, NULL);
+	if (num >= 2) {
+		if (wcscmp(cmds[1], L"o") == 0) {
+			WideCharToMultiByte(CP_ACP, 0, cmds[2], -1, m, sizeof(m), NULL, NULL);
 
-            OPEN(m);
-        }
+			OPEN(m);
+		}
 
-        if (wcscmp(cmds[1], L"home") == 0) {
-            printf("Hello, World!\n\n");
-            printf("==================================================================\n");
-            printf("          ____                                                \n");
-            printf("        ,'  , `.                                              \n");
-            printf("     ,-+-,.' _ |   ,--,                                       \n");
-            printf("  ,-+-. ;   , || ,--.'|       ,---.         ,---,             \n");
-            printf(" ,--.'|'   |  ;| |  |,       '   ,'\\    ,-+-. /  |            \n");
-            printf("|   |  ,', |  ': `--'_      /   /   |  ,--.'|'   |    ,---.   \n");
-            printf("|   | /  | |  || ,' ,'|    .   ; ,. : |   |  ,'' |   /     \\  \n");
-            printf("'   | :  | :  |, '  | |    '   | |: : |   | /  | |  /    /  | \n");
-            printf(";   . |  ; |--'  |  | :    '   | .; : |   | |  | | .    ' / | \n");
-            printf("|   : |  | ,     '  : |__  |   :    | |   | |  |/  '   ;   /| \n");
-            printf("|   : '  |/      |  | '.'|  \\   \\  /  |   | |--'   '   |  / | \n");
-            printf(";   | |`-'       ;  :    ;   `----'   |   |/       |   :    | \n");
-            printf("|   ;/           |  ,   /             '---'         \\   \\  /  \n");
-            printf("'---'             ---`-'                             `----'   \n");
-            printf("==================================================================");
-        }
-    }
-    else if (num == 1) {
-        OPEN("index.mio");
-        printf("a... you might find page is here => 'mione home'\n\n");
-    }
+		if (wcscmp(cmds[1], L"home") == 0) {
+			printf("Hello, World!\n\n");
+			printf("==================================================================\n");
+			printf("          ____                                                \n");
+			printf("        ,'  , `.                                              \n");
+			printf("     ,-+-,.' _ |   ,--,                                       \n");
+			printf("  ,-+-. ;   , || ,--.'|       ,---.         ,---,             \n");
+			printf(" ,--.'|'   |  ;| |  |,       '   ,'\\    ,-+-. /  |            \n");
+			printf("|   |  ,', |  ': `--'_      /   /   |  ,--.'|'   |    ,---.   \n");
+			printf("|   | /  | |  || ,' ,'|    .   ; ,. : |   |  ,'' |   /     \\  \n");
+			printf("'   | :  | :  |, '  | |    '   | |: : |   | /  | |  /    /  | \n");
+			printf(";   . |  ; |--'  |  | :    '   | .; : |   | |  | | .    ' / | \n");
+			printf("|   : |  | ,     '  : |__  |   :    | |   | |  |/  '   ;   /| \n");
+			printf("|   : '  |/      |  | '.'|  \\   \\  /  |   | |--'   '   |  / | \n");
+			printf(";   | |`-'       ;  :    ;   `----'   |   |/       |   :    | \n");
+			printf("|   ;/           |  ,   /             '---'         \\   \\  /  \n");
+			printf("'---'             ---`-'                             `----'   \n");
+			printf("==================================================================");
+		}
+	}
+	else if (num == 1) {
+		OPEN("index.mio");
+		printf("a... you might find page is here => 'mione home'\n\n");
+	}
 
 
 
-    LocalFree(cmds);
-    return 0;
+	LocalFree(cmds);
+	return 0;
 }
 
