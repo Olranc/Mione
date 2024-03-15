4bbe64fab8af4d32745d5ed1c5eeb74d5dd545c5
ChenNini0924 <162774572+ChenNini0924@users.noreply.github.com>
2024-03-15 23:31:21 +0800

    Update prerr.h

--- C/prerr.h
+++ C/prerr.h
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include "verson.h"
 
-
-int prerr(int line, char* msg) {
-	printf("Hey! something wrong in line : %d ,it said : %s \n", line, msg);
+int prerr(int line, char* msg,int errCode) {
+	printf("錯誤項目開頭行數：%d\n錯誤輔助說明：%s \n", line, msg);
+	printf("	Mione版本：%s\n",mione_verson);
+	printf("	錯誤代碼：'%d'\n", errCode);
 	return 1;
 }
