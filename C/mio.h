30646d1f966dc7c206117f7e1ae358fd129b3d65
奈奈 & 99 <162774572+ChenNini0924@users.noreply.github.com>
2024-03-19 21:14:19 +0800

    Update mio.h

--- C/mio.h
+++ C/mio.h
@@ -1,8 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+
 #ifndef mio_h
 #define mio_h
 
-int mio() {
 
+
+int mio(char* _case,int _type) {
+	char*heads[] = {
+		"set",
+		"input",
+	};
+
+	char* prompts[] = {
+		"do",
+		"then",
+		"else",
+	};
+
+	for (int i = 0; i < sizeof(heads)/sizeof(char*); i=i+1) {
+		
+		if (strcmp(heads[i], _case)) {		
+		}
+		else {
+			printf("pair\n");
+		}
+	}
+
+	for (int i = 0; i < sizeof(prompts) / sizeof(char*); i = i + 1) {
+
+		if (strcmp(prompts[i], _case)) {
+		}
+		else {
+			printf("pair\n");
+		}
+	}
 }
 int newline() {
 
